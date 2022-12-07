59ee7809f1a43ca77915623bb70777eb1508dccf
oytyeur <55865621+oytyeur@users.noreply.github.com>
2022-12-08 01:01:29 +0300

    General form equation parameters improvement
    Added some changes for general form line equation parameters in x = const cases

--- line.py
+++ line.py
@@ -1,5 +1,5 @@
 import math
-from math import sqrt, atan2, sin, cos
+from math import inf, sqrt, atan2, sin, cos
 
 
 class Line:
@@ -15,19 +15,22 @@ class Line:
         self.y2 = y2
         # Параметры для формы y = kx + b
         if x1 == x2:
-            self.has_general = False  # флаг о возможности полной общей формы уравнения
+            self.is_x_const = True  # флаг формы y = kx + b (k != inf)
+            self.k = inf
+            self.b = None
         else:
-            self.has_general = True
+            self.is_x_const = False
             self.k = (y2 - y1)/(x2 - x1)
+            self.b = y1 - self.k * x1
 
-        self.b = y1 - ((y2 - y1)/(x2 - x1)) * x1
         # Параметры для общей формы Ax + By + C = 0
-        if self.has_general:
+        if not self.is_x_const:
             self.A = self.k
             self.B = -1
             self.C = self.b
         else:  # форма x = const
             self.A = -1
+            self.B = 0
             self.C = x1
         # длина отрезка
         self.length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -37,16 +40,18 @@ class Line:
     # Метод расчёта расстояния от точки до прямой (как раз для этого и нужна общая форма уравнения прямой)
     def pt_dist(self, pt):
         xp, yp = pt
-        if self.has_general:
-            distance = abs(xp*self.A + yp*self.B + self.C)/sqrt(self.A ** 2 + self.B ** 2)
-        else:
-            distance = abs(xp - self.C)
+        # if self.is_x_dependent:
+        #     distance = abs(xp*self.A + yp*self.B + self.C)/sqrt(self.A ** 2 + self.B ** 2)
+        # else:
+        #     distance = abs(xp - self.C)
+        distance = abs(xp * self.A + yp * self.B + self.C) / sqrt(self.A ** 2 + self.B ** 2)
         return distance
 
     # Метод, возвращающий значение y по значению x
     def get_y(self, x):
-        if self.has_general:
+        if not self.is_x_const:
             y = self.k * x + self.b
+            return y
         else:
             return None
 
@@ -65,3 +70,10 @@ class Line:
         pts_y.append(self.y2)
 
         return pts_x, pts_y, pts_num + 1  # плюс закреплённая конечная точка
+
+
+# ln = Line((1, 0), (1, 1))
+# print(ln.k, ln.b, ln.A, ln.B, ln.C)
+# print(ln.pt_dist((3,1)))
+# ln = Line((0, 0), (1, 1))
+# print(ln.k, ln.b, ln.A, ln.B, ln.C)
