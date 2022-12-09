1a8744b6691a32a825ce61915b98ca22320311b3
oytyeur <55865621+oytyeur@users.noreply.github.com>
2022-12-09 22:34:17 +0300

    Add visualization and interactive plot with button
    Button click changes initial dataset to be approximated

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import matplotlib.pyplot as plt
+from trajectoryGenerator import TrajectoryGenerator
+from dataGenerator import DataGenerator
+from straightLineEstimatorLSM import StraightLineEstimatorLSM
+from matplotlib.widgets import Button, Slider
 
 
-# Press the green button in the gutter to run the script.
+global ln_seg
+global mess
+global tol
+
+global fig
+global ax
+
+
+def plot_data(data_generator):
+    ax.plot(data_generator.messed_pts_x, data_generator.messed_pts_y, '.', markersize=5, color='0.6')
+    plt.draw()
+
+
+def plot_results(corners):
+    start_pt = corners[0]
+    end_pt = corners[-1]
+    for i in range(len(corners) - 1):
+        x1 = corners[i][0]
+        y1 = corners[i][1]
+        x2 = corners[i + 1][0]
+        y2 = corners[i + 1][1]
+        ax.plot([x1, x2], [y1, y2])
+    ax.plot(start_pt[0], start_pt[1], 'r.')
+    ax.plot(end_pt[0], end_pt[1], 'b.')
+
+
+def approximate_new_traj(event):
+    ax.clear()
+
+    traj_generator = TrajectoryGenerator(ln_seg)
+    data_generator = DataGenerator(traj_generator, mess)
+    plot_data(data_generator)
+
+    lsm = StraightLineEstimatorLSM(data_generator)
+    lsm.estimate_traj(tol)
+    traj_corners = lsm.calc_traj_corners()
+    plot_results(traj_corners)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    ln_seg = 0.1  # периодичность "съёма измерений"
+    mess = 0.2  # мера зашумлённости
+    tol = 0.05  # допустимый порог вхождения точки в пределы отрезка
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # tg = TrajectoryGenerator(ln_seg)  # генерация траектории
+
+    # dg = DataGenerator(tg, mess)  # генерация зашумлённых данных
+
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(right=0.8, bottom=0.3)
+
+    approximate_new_traj(None)
+
+    next_btn_ax = plt.axes([0.85, 0.4, 0.1, 0.05])
+    next_btn = Button(next_btn_ax, 'Next')
+
+    next_btn.on_clicked(approximate_new_traj)
+
+    plt.show()
